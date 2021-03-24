a5ab0c720132c00c0228f7844d5016a799f4577b
Daniele Potertì <daniele.poterti@gmail.com>
2021-03-24 18:06:41 +0100

    Initial commit

---
+++ lib/providers/auth.dart
@@ -0,0 +1,122 @@
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import '../models/http_exception.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Auth with ChangeNotifier {
+  String _token;
+  DateTime _expiryDate;
+  String _userId;
+  Timer _authTimer;
+
+  bool get isAuth {
+    return token != null;
+  }
+
+  String get userId {
+    return _userId;
+  }
+
+  String get token {
+    if (_expiryDate != null &&
+        _expiryDate.isAfter(DateTime.now()) &&
+        _token != null) {
+      return _token;
+    }
+    return null;
+  }
+
+  Future<void> _autenticate(
+      String email, String password, String urlSegment) async {
+    final url = Uri.https(
+      'identitytoolkit.googleapis.com',
+      '$urlSegment',
+      {'key': 'AIzaSyBJC-r7HavX4ph7htb-wnizqmMbNuQiEeM'},
+    );
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {'email': email, 'password': password, 'returnSecureToken': true},
+        ),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      } else {
+        _token = responseData['idToken'];
+        _userId = responseData['localId'];
+        _expiryDate = DateTime.now().add(
+          Duration(
+            seconds: int.parse(responseData['expiresIn']),
+          ),
+        );
+        _autoLogout();
+        print(response);
+        notifyListeners();
+        final prefs = await SharedPreferences.getInstance();
+        final userData = json.encode(
+          {
+            'token': _token,
+            'userId': _userId,
+            'expiryDate': _expiryDate.toIso8601String(),
+          },
+        );
+        prefs.setString('userData', userData);
+      }
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<void> singup(String email, String password) async {
+    return _autenticate(email, password, 'v1/accounts:signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _autenticate(email, password, 'v1/accounts:signInWithPassword');
+  }
+
+  Future<bool> tryAutoLogin() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (!prefs.containsKey('userData')) {
+      return false;
+    }
+    final extractedUserData =
+        json.decode(prefs.getString('userData')) as Map<String, Object>;
+    final expiryDate = DateTime.parse(extractedUserData['expiryDate']);
+    if (expiryDate.isBefore(DateTime.now())) {
+      return false;
+    }
+    _token = extractedUserData['token'];
+    _userId = extractedUserData['userId'];
+    _expiryDate = expiryDate;
+    notifyListeners();
+    _autoLogout();
+    return true;
+  }
+
+  Future<void> logout() async {
+    _token = null;
+    _userId = null;
+    _expiryDate = null;
+    if (_authTimer != null) {
+      _authTimer.cancel();
+      _authTimer = null;
+    }
+    notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    // prefs.remove('userData');
+    prefs.clear();
+  }
+
+  void _autoLogout() {
+    if (_authTimer != null) {
+      _authTimer.cancel();
+    }
+    final timeToExpiry = _expiryDate.difference(DateTime.now()).inSeconds;
+    _authTimer = Timer(Duration(seconds: timeToExpiry), logout);
+  }
+}
---
+++ lib/screens/orders_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/orders.dart' show Orders;
+import '../widgets/order_item.dart';
+import '../widgets/app_drawer.dart';
+
+class OrdersScreen extends StatefulWidget {
+  static const routeName = '/orders';
+
+  @override
+  _OrdersScreenState createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  Future _ordersFuture;
+  Future _obtainOrdersFuture() {
+    return Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+  }
+
+  @override
+  void initState() {
+    _ordersFuture = _obtainOrdersFuture();
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // final ordersData = Provider.of<Orders>(context);
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Your Orders'),
+        ),
+        drawer: AppDrawer(),
+        body: FutureBuilder(
+          future: _ordersFuture,
+          builder: (ctx, dataSnapshot) {
+            if (dataSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              if (dataSnapshot.error != null) {
+                //error
+                return Center(
+                  child: Text('An error occurred!'),
+                );
+              } else
+                return Consumer<Orders>(
+                  builder: (ctx, ordersData, child) => ListView.builder(
+                    itemBuilder: (ctx, i) => OrderItem(ordersData.orders[i]),
+                    itemCount: ordersData.orders.length,
+                  ),
+                );
+            }
+          },
+        ));
+  }
+}
