8b3568d35f248549596a3f8b4b321378108b1f0c
Daniele Potertì <daniele.poterti@gmail.com>
2021-03-30 11:32:09 +0200

    282

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -115,22 +115,22 @@ class _AuthCardState extends State<AuthCard>
         milliseconds: 300,
       ),
     );
-   
+
     _heightAnimation = Tween<Size>(
             begin: Size(double.infinity, 260), end: Size(double.infinity, 320))
         .animate(
       CurvedAnimation(parent: _controller, curve: Curves.linear),
     );
-    _heightAnimation.addListener(() {
-      setState(() {});
-    });
+    // _heightAnimation.addListener(() {
+    //   setState(() {});
+    // });
   }
 
   @override
-    void dispose() {
-      super.dispose();
-      _controller.dispose();
-    }
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
 
   void _showErrorDialog(String message) {
     showDialog(
@@ -218,12 +218,16 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        // height: _authMode == AuthMode.Signup ? 320 : 260,
-        height: _heightAnimation.value.height,
-        constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (ctx, ch) => Container(
+          // height: _authMode == AuthMode.Signup ? 320 : 260,
+          height: _heightAnimation.value.height,
+          constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
+          width: deviceSize.width * 0.75,
+          padding: EdgeInsets.all(16.0),
+          child: ch,
+        ),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
