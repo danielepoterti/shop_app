f2f154a6b394c9bc155c088e00ef103e99813d24
Daniele Potertì <daniele.poterti@gmail.com>
2021-03-30 16:10:25 +0200

    287

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -27,12 +27,15 @@ class ProductItem extends StatelessWidget {
               arguments: product.id,
             );
           },
-          child: FadeInImage(
-            placeholder: AssetImage(
-              'assets/images/product-placeholder.png',
+          child: Hero(
+            tag: product.id,
+            child: FadeInImage(
+              placeholder: AssetImage(
+                'assets/images/product-placeholder.png',
+              ),
+              image: NetworkImage(product.imageUrl),
+              fit: BoxFit.cover
             ),
-            image: NetworkImage(product.imageUrl),
-            fit: BoxFit.cover
           ),
         ),
         footer: GridTileBar(
